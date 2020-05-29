3a42ba6abd6a03575f782a5300646ba347e13998
gojoise <mathieu.benard@u-psud.fr>
2020-05-29 19:10:28 +0200

    un début pour artéfact plus images ajoutés

--- src/fr/forbidden_island/data/Artefact.java
+++ src/fr/forbidden_island/data/Artefact.java
@@ -1,5 +1,22 @@
 package fr.forbidden_island.data;
 
 public class Artefact {
-int a= 42;
+
+	private int absc;
+	private int ord;
+
+	public int getAbsc() {
+		return this.absc;
+	}
+	public void setAbsc(int x) {
+		this.absc=x;
+	}
+
+	public int getOrd() {
+		return this.ord;
+	}
+
+	public void setOrd(int y) {
+		this.ord=y;
+	}
 }
