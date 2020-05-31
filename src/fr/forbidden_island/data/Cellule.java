d6e54e456aa3274d3438c0fc3bc1635d43ed3d03
arthur clot <arthur.clot@u-psud.fr>
2020-06-01 01:36:24 +0200

    cellule clear

--- src/fr/forbidden_island/data/Cellule.java
+++ src/fr/forbidden_island/data/Cellule.java
@@ -1,84 +1,93 @@
 package fr.forbidden_island.data;
 
-
 import fr.forbidden_island.core.Model;
 
-//
-public class Cellule{
+
+public class Cellule {
 	
-	private final int x, y; //position de la cellule sur la grille
-	private Model modele;
-	public  static int size;
+	public static int size;
 	private TypeTerrain typeTerrainC;
-	//attribut image;
-	private Joueur jx;
-	/**classe cellule :
-	 *attribut terrain/joueur/special(artefact clef/element/heliport)
-	 * 
-	 * sous classe terrain:
-	 * attribut  (booleen) terre/mer . (mer definitif)
-	 * attribut booleen (avec condition terre) inonde'/normal submerge' (deviens mer) .
-	 * 
-	 * sous classe special
-	 * artefacts / clef/element/heliport )
-	 */
 	
-	public Cellule(Model mod,int x,int y) {
-		//charge l' images depuis classe image dans cette cellule
-		this.x=x;
-		this.y=y;
-		this.modele=mod;
+	// coords
+	private final int x, y; // position de la cellule sur la grille
+	
+	//nécessaire pour hasJoueurs() et hasArtefact()
+	private Model modele;
+
+	public Cellule(Model mod, int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.modele = mod;
 	}
-	
+	 
 	/**
-	 * 
-	 * @param la grille !!
-	 * @return
+	 * renvoi un tableau de 4 cellules voisines de la cellule
+	 * @param grille
+	 * @return le tableau de 4 cellules
 	 */
-	public Cellule [] voisines(Cellule [][] grille) {
-		//if(this.estMer)throw new IllegalArgumentException(); 
-		Cellule [] res=new Cellule[4];
-		int x=this.getAbsc();
-		int y=this.getOrd();
-		res[0]=grille[x][y-1];//haut
-		res[1]=grille[x+1][y];//droite
-		res[2]=grille[x][y+1];//bas
-		res[3]=grille[x-1][y];//gauche
+	public Cellule[] voisines(Cellule[][] grille) {
+		Cellule[] res = new Cellule[4];
+		int x = this.getAbsc();
+		int y = this.getOrd();
+		res[0] = grille[x][y - 1];// haut
+		res[1] = grille[x + 1][y];// droite
+		res[2] = grille[x][y + 1];// bas
+		res[3] = grille[x - 1][y];// gauche
 
 		return res;
-		}
-
-	
-
-	
-	public int getAbsc () {return this.x;}
-	
-	public int getOrd () {return this.y;}
-	
-	public static int getSize () {return size;}
-
-	public void setTypeTerrain (TypeTerrain t) {this.typeTerrainC=t;}
-	
-	public static void setSize (int s) {size=s;}
-	
-	public TypeTerrain getTypeTerrain () {return this.typeTerrainC;}
-	
-	public boolean hasJoueur(){
-		for(Joueur joueur : modele.joueurs) {
-			if(joueur.getAbsc()==this.x && joueur.getOrd()==this.y && joueur.getStatut()==Statut.vivant)	
-					return true;
+	}
+	/**parcours le tableau de joueurs de modele et regarde si un de ces joueurs se trouve sur la cellule
+	 * attention, le joueur doit etre vivant.
+	 * @return boolean selon si il y a ou pas un joueur sur la cellule
+	 */
+	public boolean hasJoueur() {
+		for (Joueur joueur : modele.joueurs) {
+			if (joueur.getAbsc() == this.x && joueur.getOrd() == this.y && joueur.getStatut() == Statut.vivant)
+				return true;
 		}
 		return false;
 	}
-	 
-	public boolean hasArtefact(){
-			for(Artefact artefact : modele.artefacts) {
-				if(artefact!=null) {
-				if(artefact.getAbsc()==this.x && artefact.getOrd()==this.y && !artefact.hasProprio())			
+	/**parcours le tableau d'artefacts de modele et regarde si un de ces artefacts se trouve sur la cellule
+	 * attention, l'artefact doit etre sans proriétaire (proprio).
+	 * @return boolean selon si il y a ou pas un artefact sur la cellule
+	 */
+	public boolean hasArtefact() {
+		for (Artefact artefact : modele.artefacts) {
+			if (artefact != null) {
+				if (artefact.getAbsc() == this.x && artefact.getOrd() == this.y && !artefact.hasProprio())
 					return true;
 			}
 		}
 		return false;
 	}
 	
+	
+///////////////////des GETs///////////////////////
+
+	public int getAbsc() {
+		return this.x;
+	}
+
+	public int getOrd() {
+		return this.y;
+	}
+	
+	public static int getSize() {
+		return size;
+	}
+
+	public TypeTerrain getTypeTerrain() {
+		return this.typeTerrainC;
+	}
+	
+///////////////////des SETs///////////////////////
+	
+	public static void setSize(int s) {
+		size = s;
+	}
+	
+	public void setTypeTerrain(TypeTerrain t) {
+		this.typeTerrainC = t;
+	}
+
 }
