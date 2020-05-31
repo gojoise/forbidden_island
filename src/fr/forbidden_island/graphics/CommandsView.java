f445dd2c3b9455eafce6fdc40bf6112ce69417ae
gojoise <mathieu.benard@u-psud.fr>
2020-06-01 01:52:26 +0200

    CommandsView docs and clean

--- src/fr/forbidden_island/graphics/CommandsView.java
+++ src/fr/forbidden_island/graphics/CommandsView.java
@@ -3,18 +3,16 @@ package fr.forbidden_island.graphics;
 import javax.swing.JButton;
 import fr.forbidden_island.Controls.Controler;
 import fr.forbidden_island.core.Model;
-
+/**
+ * Classe affichant le bouton et servant de lien avec le controleur
+ */
 public class CommandsView extends JButton {
 
 	public CommandsView(Model mod){
 		Controler ctrl = new Controler(mod);
-		/** Enregistrement du contro'leur comme auditeur du bouton. */
-		this.addActionListener(ctrl);
-		this.addKeyListener(ctrl);
-		this.setText("fin de tour !");
+		/** Enregistrement du controleur comme auditeur du bouton. */
+		this.addActionListener(ctrl);//Le controleur ecoute les clics du bouton
+		this.addKeyListener(ctrl);//Le controleur ecoute les frappes de clavier
+		this.setText("Fin de tour !");//Affiche les texte du boutton
 	}
-
-
-
-
 }
