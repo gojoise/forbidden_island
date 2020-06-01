eb2aeac833e583f1ebf7c14074b0242c5d52c694
gojoise <mathieu.benard@u-psud.fr>
2020-06-01 02:19:49 +0200

    main docs and clean

--- src/fr/forbidden_island/core/Main.java
+++ src/fr/forbidden_island/core/Main.java
@@ -2,13 +2,13 @@ package fr.forbidden_island.core;
 import fr.forbidden_island.data.Ressources;
 import fr.forbidden_island.graphics.*;
 
-
+/**
+ * Main Class with main method for the programm 
+ */
 public class Main {
 	public static void main(String[] args) {
-		Ressources ress= new Ressources();
-		Fenetre f=new Fenetre(ress);
-		Acceuil a = new Acceuil(f, "Lancer la partie", true,ress); //new Vue	
-		
-		
+		Ressources ress= new Ressources();//Chargement des ressources i.e des images
+		Fenetre f=new Fenetre(ress);//Creation d'une fenetre vide
+		Acceuil a = new Acceuil(f, "Lancer la partie", true,ress);//Creation de la boite de dialogue permettant de faire varier les paramètres
 	}
 }
