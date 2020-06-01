0d83ccaeb0f5054c49e90b86fcc6b00db8d12673
gojoise <mathieu.benard@u-psud.fr>
2020-06-01 03:02:28 +0200

    minor change in main

--- src/fr/forbidden_island/core/Main.java
+++ src/fr/forbidden_island/core/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		Ressources ress= new Ressources();
 		Fenetre f=new Fenetre(ress);
-		Acceuil a = new Acceuil(f, "Lancer la partie", true,ress); //new Vue	
+		new Acceuil(f, "Lancer la partie", true,ress); //new Vue	
 		
 		
 	}
